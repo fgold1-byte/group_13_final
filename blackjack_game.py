dd51aba3254154a1cd6d4644214039162c8e78b3
Suhaib Wazihuddin <suhaib.wazi@gmail.com>
2026-04-24 11:10:57 -0400

    dos strings

--- blackjack_game.py
+++ blackjack_game.py
@@ -67,6 +67,21 @@ class Hand:
             aces -= 1
             
         return self.total
+    
+    def get_power_suit(self):
+        """
+        The suit of the first card
+        
+        Returns(str): The first card's suit, or none if hand is empty
+        
+        Author: Wazihuddin
+        """
+        if len(self.cards) > 0:
+            return self.cards[0].suit
+        return None
+    
+    def __str__():
+        pass
 
 class Player:
     """
@@ -76,21 +91,19 @@ class Player:
     name(Str): the players name
     chips(int): starting number of chips
     
-    Author: Isha 
+    Author: Isha
     """
     def __init__(self, name: str, chips: int):
         self.name = name
         self.chips = chips
         self.hand = []
         self.next_round_stats = {}
-        
         """
         test
         1
         2
         3
         """
-        
     def place_bet():
         """
         deducts from the players chip balance, applying any club discount
@@ -121,18 +134,81 @@ class Bet:
         pass
     
     def resolve_outcome():
+        """
+        Compute the round payout using blackjack rules and first-card suit power
+        
+        Orignal algorithm steps:
+        1. get the players total and dealers total
+        2. check if the player busted
+        3. check if the dealer busted or if the player won
+        4. check if the result is a push.
+        5. apply the first-card suit power-up
+        6. return the final payout
+        
+        args: player_hand & dealer_hand
+        
+        author: 
+        """
+        
         pass
 
 def blackjack_check():
+    """
+    check weather a hand is a blackjack
+    
+    args: hand: evaluating the hand
+    
+    Returns: bool: true if the hand has two cards that add up to 21, false otherwise
+    
+    author:
+    """
     pass
 
+def player_turn():
+    """
+    player turn
+    
+    args:
+        player: the player taking the turn
+        deck: the current deck
+        
+    author:
+    """
+    
+    pass
 def dealer_turn():
+    """
+    run the dealers turn
+    
+    args:
+        dealer: the dealer
+        deck: the currnt deck
+        
+    author:
+    """
     pass                
 
 def apply_club_discount():
+    """apply the clubs power-up if the players first card is a club
+    
+    args: player: the player recives a discount
+    
+    author:
+    
+    """
     pass
 
 def reveal_hidden_card_spade():
+    """
+    reveal dealers hidden card if the players first card is a spade
+    
+    args:
+        player: the player
+        dealer: the dealer
+        
+    author:
+    """
+    
     pass
 
 def main():
